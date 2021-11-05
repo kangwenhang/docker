aa90628f4ecfc95122ceb5b320e1dfad4219a081
kangwenhang <kangwenhang@hotmail.com>
2021-11-06 02:39:42 +0800

    update

--- shell/push.sh
+++ shell/push.sh
@@ -410,58 +410,7 @@ function Yes_Open {
   echo -e "\n=============================项目确认完成==============================\n"
 }
 
-#获取仓库更新日志
-function Git_log {
-  if [ "$diy_commit" = "" ]; then
-    echo "未设置自定义提交内容，使用系统级识别日志"
-    grep ^[A].* $submit/commit.log 2>&1 | tee $submit/A.log
-    grep ^[C].* $submit/commit.log 2>&1 | tee $submit/C.log
-    grep ^[D].* $submit/commit.log 2>&1 | tee $submit/D.log
-    grep ^[M].* $submit/commit.log 2>&1 | tee $submit/M.log
-    grep ^[R].* $submit/commit.log 2>&1 | tee $submit/R.log
-    grep ^[T].* $submit/commit.log 2>&1 | tee $submit/T.log
-    grep ^[U].* $submit/commit.log 2>&1 | tee $submit/U.log
-    grep ^[X].* $submit/commit.log 2>&1 | tee $submit/X.log
-    cat /dev/null > $submit/commit.log
-    for z in `ls $submit`;do
-      if [ "$z" != "$submit/commit.log" ];then
-        if test -s $submit/$z;then
-          awk '{$1="";print $0}' $submit/$z 2>&1 | tee $submit/push.log
-          sed '1s/^/[/;$!s/$/,/;$s/$/]/' $submit/push.log 2>&1 | tee $submit/$z
-          cat $submit/$z | xargs 2>&1 | tee $submit/push.log
-          cat $submit/push.log 2>&1 | tee $submit/$z
-        fi
-      fi
-    done
-    if [ -e "$submit/A.log" ] && test -s $submit/A.log;then
-      sed 's/^/新增：/' $submit/A.log 2>&1 | tee -a $submit/commit.log
-    fi
-    if [ -e "$submit/C.log" ] && test -s $submit/C.log;then
-      sed 's/^/拷贝：/' $submit/C.log 2>&1 | tee -a $submit/commit.log
-    fi
-    if [ -e "$submit/D.log" ] && test -s $submit/D.log;then
-      sed 's/^/删除：/' $submit/D.log 2>&1 | tee -a $submit/commit.log
-    fi
-    if [ -e "$submit/M.log" ] && test -s $submit/M.log;then
-      sed 's/^/修改内容：/' $submit/M.log 2>&1 | tee -a $submit/commit.log
-    fi
-    if [ -e "$submit/R.log" ] && test -s $submit/R.log;then
-      sed 's/^/修改文件名：/' $submit/R.log 2>&1 | tee -a $submit/commit.log
-    fi
-    if [ -e "$submit/T.log" ] && test -s $submit/T.log;then
-      sed 's/^/文件类型修改：/' $submit/T.log 2>&1 | tee -a $submit/commit.log
-    fi
-    if [ -e "$submit/U.log" ] && test -s $submit/U.log;then
-      sed 's/^/未合并：/' $submit/U.log 2>&1 | tee -a $submit/commit.log
-    fi
-    if [ -e "$submit/X.log" ] && test -s $submit/X.log;then
-      sed 's/^/状态错误：/' $submit/X.log 2>&1 | tee -a $submit/commit.log
-    fi
-    diy_commit=$(cat $submit/commit.log)
-  else
-    echo "已设置提交内容，进行下一步"
-  fi
-}
+
 
 #上传文件至github
 function Push_github {
@@ -476,7 +425,7 @@ function Push_github {
     git status --short | tee $submit/commit.log
     if test -s $submit/commit.log;then
       echo "文件存在变更，正常上传"
-      Git_log
+      source /push/shell/submit.sh > $submit/1.log
       git config user.name "$diy_user_name"
       git config user.email "$diy_user_email"
       git commit --allow-empty -m "$diy_commit"
