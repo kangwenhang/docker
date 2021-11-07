bc978bf0f31330ae23ca6a28879a4440bc7559d5
kangwenhang <kangwenhang@hotmail.com>
2021-11-07 11:22:12 +0800

    update

--- shell/run.sh
+++ shell/run.sh
@@ -7,6 +7,39 @@ source /push/shell/share.sh
 source $file_config
 
 ##运行脚本
-mkdir -p $diy_logs
-source $shell_model/push.sh 2>&1 | tee $log_path
-exit
+function run_sh {
+  mkdir -p $diy_logs
+  source $shell_model/push.sh 2>&1 | tee $log_path
+  exit
+}
+
+function run_sh_sd {
+  cp -rf $config/crontab.list $config/crontab.list.back
+  awk '{print "#"$0}' $config/crontab.list
+  mkdir -p $diy_logs
+  source $shell_model/push.sh 2>&1 | tee $log_path
+  cp -rf $config/crontab.list.back $config/crontab.list
+  exit
+}
+
+case $# in
+  0)
+    echo
+  1)
+    if [[ $1 == sd ]]; then
+      run_sh_sd $1
+    else
+      run_sh $1
+    fi
+    ;;
+  2)
+    if [[ $2 == sd ]]; then
+      run_sh_sd $1
+    else
+      run_sh $1
+    fi
+    ;;
+  *)
+    echo -e "\n命令过多...\n"
+    ;;
+esac
