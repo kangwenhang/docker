22a29b0615aace3e1c6617ef198f9f1769aee21e
kangwenhang <kangwenhang@hotmail.com>
2021-11-07 23:05:38 +0800

    update

--- shell/share.sh
+++ shell/share.sh
@@ -11,7 +11,7 @@ diy_logs=$logs/$config_use
 log_time=$(date "+%Y-%m-%d-%H-%M-%S.%N")
 log_path="$diy_logs/$log_time.log"
 ListCron=$config/crontab.list
-submit=$diy_logs/submit
+submit=$diy_logs/sd/submit
 
 #push专用变量
 diy_config=$dir_root/diy/$config_use
