cba63892379ef0b6c11d7598ebe9624fa669d6bd
kangwenhang <kangwenhang@hotmail.com>
2021-11-07 15:46:25 +0800

    update

--- shell/run.sh
+++ shell/run.sh
@@ -22,26 +22,12 @@ function run_sh_sd {
   exit
 }
 
-case $# in
-  0)
-    echo
-  1)
-    if [[ $1 == sd ]]; then
-      config_use=config
-      run_sh_sd
-    else
-      config_use=config
-      run_sh
-    fi
-    ;;
-  2)
-    if [[ $2 == sd ]]; then
-      run_sh_sd $1
-    else
-      run_sh $1
-    fi
-    ;;
-  *)
-    echo -e "\n命令过多...\n"
-    ;;
-esac
+
+if [[ $1 == sd ]]; then
+    config_use=config
+    run_sh_sd
+elif [[ $2 == sd ]]; then
+    run_sh_sd $1
+else
+    run_sh $1
+fi
